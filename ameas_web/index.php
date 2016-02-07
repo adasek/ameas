f0af630c9ed2b07ec1ce23880257f72cdc6a6bf2
Adam Benda <adam@adasek.cz>
2016-02-07 10:24:44 +0100

    Cleanup

--- ameas_web/index.php
+++ ameas_web/index.php
@@ -1,34 +1,33 @@
-<?php                                  
+<?php
+
 include_once('config.php');
 include_once('include/connection.php');
 include_once('include/login.php');
 
-$tpl_content="";
-$tpl_title="Ameas";
-            
+$tpl_content = "";
+$tpl_title = "Ameas";
 
-if(!$logged){      
-  //We are not logged = show login form     
-  include('include/login_form.php'); 
-  
-  include_once('include/template.php');
-  exit;     
-}                 
 
-if($_GET['w'] === "sensors"){
- include('include/sensors.php');
+if (!$logged) {
+    //We are not logged = show login form     
+    include('include/login_form.php');
+
+    include_once('include/template.php');
+    exit;
 }
-else if($_GET['w'] === "values"){
- include('include/values.php');
-}else if($_GET['w'] === "plot"){
- include('include/plot.php');
-}else{
- // MENU
- $tpl_content.="<ul>";
- $tpl_content.="<li><a href=\"".$BASEHREF."?w=sensors\">Sensors</a></li>\n";
- $tpl_content.="<li><a href=\"".$BASEHREF."?w=values\">Values</a></li>\n"; 
- $tpl_content.="</ul>\n";
 
+if ($_GET['w'] === "sensors") {
+    include('include/sensors.php');
+} else if ($_GET['w'] === "values") {
+    include('include/values.php');
+} else if ($_GET['w'] === "plot") {
+    include('include/plot.php');
+} else {
+    // MENU
+    $tpl_content.="<ul>";
+    $tpl_content.="<li><a href=\"" . $BASEHREF . "?w=sensors\">Sensors</a></li>\n";
+    $tpl_content.="<li><a href=\"" . $BASEHREF . "?w=values\">Values</a></li>\n";
+    $tpl_content.="</ul>\n";
 }
 
 
