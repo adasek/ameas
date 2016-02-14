cfc40252819e0686205026c7f707a998e6dcca5c
Adam Benda <adam@adasek.cz>
2016-02-14 16:32:32 +0100

    Added SensorType data to database-import sql file

--- asensor-server/ameas_db.sql
+++ asensor-server/ameas_db.sql
@@ -64,6 +64,12 @@ CREATE TABLE `SensorType` (
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_czech_ci;
 
+INSERT INTO `SensorType` (`id`, `name`, `script`, `retries`, `numOfRemeasurements`) VALUES
+(1,	'Dallas DS18B20',	'/sbin/measure_dalas',	3,	10),
+(2,	'DHT22',	'/sbin/measure_dht22',	10,	10),
+(3,	'BMP180',	'/sbin/measure_bmp180',	10,	10),
+(4,	'PCF8591',	'/sbin/measure_pcf8591',	3,	3),
+(5,	'BH1750FVI',	'/sbin/measure_bh1750fvi',	5,	2);
 
 DROP TABLE IF EXISTS `sensorType-valueType`;
 CREATE TABLE `sensorType-valueType` (
@@ -76,6 +82,14 @@ CREATE TABLE `sensorType-valueType` (
   CONSTRAINT `FK_ASS_9` FOREIGN KEY (`ValueType_id`) REFERENCES `ValueType` (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_czech_ci;
 
+INSERT INTO `sensorType-valueType` (`SensorType_id`, `ValueType_id`, `pos`) VALUES
+(1,	1,	1),
+(2,	1,	2),
+(2,	2,	1),
+(3,	1,	1),
+(3,	3,	2),
+(4,	4,	1),
+(5,	5,	1);
 
 DROP TABLE IF EXISTS `User`;
 CREATE TABLE `User` (
@@ -96,5 +110,11 @@ CREATE TABLE `ValueType` (
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_czech_ci;
 
+INSERT INTO `ValueType` (`id`, `shortName`, `name`, `allowedDeviation`, `color`) VALUES
+(1,	'temperature',	'Temperature [°C]',	1,	'#ff0000'),
+(2,	'hum',	'Relative humidity[%]',	4,	'#0000ff'),
+(3,	'air pressure',	'Air pressure [Pa]',	20,	'#3399dd'),
+(4,	'gas',	'concentration CO,H2,LPG,CH4,alcohol,smoke,propane',	5,	'#330000'),
+(5,	'lux',	'Light [lux]',	100000000,	'#00aaaa');
 
--- 2016-02-14 13:59:43
+-- 2016-02-14 15:29:40
